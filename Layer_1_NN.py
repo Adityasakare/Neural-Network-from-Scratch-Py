54b500a141565311f0b531fc6fb34c66600bb107
Aditya Sakare <aditya.sakare16@gmail.com>
2020-06-17 20:38:47 +0530

    Basic Neural Network

---
+++ Layer_1_NN.py
@@ -0,0 +1,11 @@
+X = [1.1, 1.2, 1.3] #INPUT
+
+W_1 = [2.1, 2.2, -2.3] #WEIGHT1
+
+B = 1                 #BIAS
+
+#FORMULA = INPUT(n)*WEIGHT(n) + BIAS
+
+output = X[0]*W_1[0] + X[1]*W_1[1] + X[2]*W_1[2] + B
+
+print(output)
